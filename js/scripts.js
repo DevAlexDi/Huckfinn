3184053e8345a58e8ecb960973502bb99b63afcf
unknown <keitmark@mail.ru>
2018-05-19 14:25:43 +0300

    menu almost done

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
     
     
     $('.slider-init').slick({
-        dots: false,
+        dots: true,
         arrows: true,
         infinite: false,
-        slidesToShow: 1
-
+        slidesToShow: 1,
+         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 700
+    });
+    
+    $('.click-modal-xs').click(function(){
+        $('#modal-menu').modal('show');
+        
     });
     
     
